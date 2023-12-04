bbb23a6d2c15b0fa1fc182357c3cf41d41cbfed7
mischa <mbehari@udel.edu>
2023-12-03 20:55:52 -0500

    got rid of extra unnecessary files.

--- welcome_page.py
+++
@@ -1,9 +0,0 @@
-from designer import *
-game_name = text("black", "Musical Madness", 40, 400, 100)
-
-instructions = text("black", "For this game, you will be tested on how well you know your scales!", 20, 400, 300)
-instructions2 = text("black", "You'll be shown different musical notes/key signatures", 20, 400, 350)
-instructions3 = text("black", "Choose which note/signature the image represents and increase your score!", 20, 400, 400)
-draw(instructions, instructions2, instructions3, game_name)
-
-
